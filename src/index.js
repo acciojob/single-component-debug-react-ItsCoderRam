159614a1c6c55db779eb80b536646b6957598bcb
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-05-27 12:57:00 +0000

    changes fine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+let container = document.getElementById('root')
+ReactDOM.render(<App />, container);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
 
 
 
